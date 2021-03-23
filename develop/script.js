3891f38ad1059e753efa1915822c3d9dc3558ab0
unknown <peter.siapkas@gmail.com>
2021-03-23 11:00:41 +1100

    finished the random password generator

--- develop/script.js
+++ develop/script.js
@@ -1,52 +1,86 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-//defines characters in array
-var specialCharacters = ["!","@","#","$","%","^","&","*"];
-var numbers = ["1","2","3","4","5","6","7","8","9","0"];
-var upperLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-var lowerLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","v","w","x","y","z" ]
-//randomises each array
-var upperIndex = Math.floor(Math.random() * upperLetters.length);
-var lowerIndex = Math.floor(Math.random() * lowerLetters.length);
-var numberIndex = Math.floor(Math.random() * numbers.length);
-var specialChar = Math.floor(Math.random() * specialCharacters.length);
-// window alerts 
-
-var specialQuestion = window.confirm ("Do you want special characters ?")
-var numQuestion = window.confirm("Do you want numbers ? ")
-var lowerQuestion = window.confirm("Do you want lower case letters ?")
-var upperQueston = window.confirm ("Do you want upper case letters ?")
-function generatePassword() {
- 
-  if (specialQuestion ) {
-    specialChar 
-  }
-  console.log (specialQuestion)
-  if (numQuestion) {
-    numberIndex
-  }
-  if (lowerQuestion) {
-    lowerIndex
-  }
-  if (upperQueston){
-    upperIndex
-  }
-
-
-
+// turns random selection into function
+function getRandomLower(){
+ return String.fromCharCode(Math.floor(Math.random() * 26 ) + 97)
 }
+
+function getRandomUpper(){
+  return String.fromCharCode(Math.floor(Math.random() * 26 ) + 65)
+}
+
+function getRandomNumber(){
+  return String.fromCharCode(Math.floor(Math.random() * 10 ) + 48)
+}
+
+function getRandomSymbols(){
+  var symbols = "<>?[}{]/=+-)(*&^%$@!"
+  return symbols[Math.floor(Math.random() * symbols.length) ]
+}
+//  turns all randomizer functions into a single variable 
+var randomSelect = {
+  lower: getRandomLower,
+  upper: getRandomUpper,
+  symbol: getRandomSymbols,
+  number: getRandomNumber
+
+};
+
+// generates password string
+function generatePassword() {
+   var number = prompt ("how long you want the password?")
+  //  if (!Number.isInteger(number)){
+  //    alert("Number required!");
+  //    return;
+  //  }
+  if (number <8){ 
+    alert ("You need a minimum of 8 characters!");
+    return;
+  } 
+  if (number >120){
+    alert("Length too long!")
+    return;
+  }
+
+
+   var functions = [ ];
+   if (confirm ("Do you want numbers ?")){
+     functions.push (randomSelect.number);
+   }
+   
+   if (confirm ("Do you want lower case letters ?")){
+     functions.push (randomSelect.lower);
+      }
+   if (confirm ("Do you want upper case letters ?")){
+    functions.push (randomSelect.upper);
+    }
+
+  if (confirm ("Do you want Symbols ?")){
+   functions.push (randomSelect.symbol);
+    }
+  if (functions.length == 0){
+    alert ("No option selected"); 
+    return;
+  }
+ 
+  var text = "";
+  var i;
+  for (i = 0; i < number; i++)  { 
+    var r = (Math.floor(Math.random() * functions.length));
+    text += functions[r]();
+   }
+  return text;
+}
+
 
 
 // Write password to the #password input
 function writePassword() {
-  console.log ("here")
-  var password = generatePassword(specialChar, numberIndex, lowerIndex, upperIndex);
   var passwordText = document.querySelector("#password");
-  console.log(numberIndex)
-  passwordText.value = password;
+  passwordText.value =  generatePassword();
   
  
-}
+};
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword); 
